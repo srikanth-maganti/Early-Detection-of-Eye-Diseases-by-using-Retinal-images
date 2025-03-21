f4b075247b79ba1f5efbbb5736dc9b00d9539886
srikanth-maganti <magantisrikanth45@gmail.com>
2025-03-21 22:55:53 +0530

    add more

--- prediction.py
+++ prediction.py
@@ -98,7 +98,7 @@ def load_model():
             model=model.to(torch.device("cuda"),non_blocking=True)
         else:
             model=model.to(torch.device("cpu"),non_blocking=True)
-        model.load_state_dict(torch.load('classifier_model.pth'))
+        model.load_state_dict(torch.load('classifier_model1.pth'))
         model.eval()
         return model
     
